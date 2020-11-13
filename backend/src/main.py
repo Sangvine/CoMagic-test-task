5c84a54e7375ec7fbe2e2f5336e474373fa2d63c
Dmitry Ryabokon <sangvinion1@gmail.com>
2020-11-13 20:23:06 +0300

    Implemented "get site details"

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,22 +1,37 @@
 import aiohttp
 from aiohttp import web
 import asyncio
+import re
 
 
-async def fetch(client):
+async def fetch_get_sites(token=None):
     url = 'https://dataapi.comagic.ru/v2.0'
     headers = {'Content-Type': 'application/json; charset=UTF-8'}
     json = {
         "jsonrpc": "2.0",
-        "id": "number",
+        "id": "0",
         "method": "get.sites",
         "params": {
             "access_token": "vy7m6ggz45gek4xrxc4i0mvof0gjoemys2efze8n",
         }
     }
-    async with client.post(url, headers=headers, json=json) as resp:
-        assert resp.status == 200
-        return await resp.json()
+    async with aiohttp.ClientSession() as client:
+        async with client.post(url, headers=headers, json=json) as resp:
+            assert resp.status == 200
+            return await resp.json()
+
+
+async def fetch_site_details(url):
+    async with aiohttp.ClientSession() as client:
+        try:
+            async with client.get(url) as resp:
+                assert resp.status == 200, "страница не отвечает"
+                return await resp.text()
+        except AssertionError as error:
+            print(error.args[0])
+            return ""
+        except:
+            return "404"
 
 
 async def hello(request):
@@ -25,18 +40,24 @@ async def hello(request):
 
 
 async def get_sites(request):
-    #data = "Hello, world!!"
-    data = []
-    async with aiohttp.ClientSession() as client:
-        html = await fetch(client)
-        data.append(html)
-
-    return web.json_response(data)
+    data = await fetch_get_sites()
+    result = []
+    for item in data["result"]["data"]:
+        result.append({"site_key": item["site_key"],
+                       "domain_name": item["domain_name"]})
+    return web.json_response(result)
 
 
 async def get_site_details(request):
-    data = "Hello, world!!"
-    return web.json_response(data)
+    site_key = request.rel_url.query.get('site_key', '')
+    cs_min = "cs.min.js"
+    domain_name = request.rel_url.query.get('domain_name', '')
+    text = await fetch_site_details("http://" + domain_name + "/")
+    is_cs_min = re.search(cs_min, text)
+    is_site_key = re.search(site_key, text)
+    result = {"404": "false", "cs.min.js": bool(is_cs_min),
+              "is_site_key": bool(is_site_key)}
+    return web.json_response(result)
 
 app = web.Application()
 routes = [web.get('/', hello),
